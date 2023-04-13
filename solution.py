6be327b2df2a90f8c39c75fa62682b4380a74898
ArtRakh <114469896+ArtRakh@users.noreply.github.com>
2023-04-13 20:01:00 +0300

    Update solution.py

--- solution.py
+++ solution.py
@@ -2,13 +2,14 @@ import pandas as pd
 import numpy as np
 
 
-chat_id = 123456 # Ваш chat ID, не меняйте название переменной
+chat_id = 1369690762 # Ваш chat ID, не меняйте название переменной
 
 def solution(x_success: int, 
              x_cnt: int, 
              y_success: int, 
              y_cnt: int) -> bool:
-    # Измените код этой функции
-    # Это будет вашим решением
-    # Не меняйте название функции и её аргументы
-    return ... # Ваш ответ, True или False
+    alpha=.06
+    if abs(x_success/x_cnt - y_success/y_cnt) > alpha:
+        return True
+    else:
+        return False
